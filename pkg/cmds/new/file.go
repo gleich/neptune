a40ecf9aaa398801401f84f8d2c34f82b396e171
Matt Gleich <git@mattglei.ch>
2021-08-01 15:12:12 -0400

    feat: implement nebula files
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- pkg/cmds/new/file.go
+++ pkg/cmds/new/file.go
@@ -1,16 +1,23 @@
 package new
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/fatih/color"
 	"github.com/gleich/neptune/pkg/out"
 	"github.com/gleich/neptune/pkg/util"
 )
+
+// Outline for a markdown file
+type markdownFile struct {
+	folder string
+	name   string
+	binary []byte
+}
 
 // Create a new markdown file
 func File() (string, error) {
@@ -25,12 +32,14 @@ func File() (string, error) {
 	}
 
 	fmt.Println()
-	var location string
+	var (
+		location string
+	)
 	switch fType {
 	case "galaxy":
-		createIn := "galaxies"
+		folder := "galaxies"
 		for {
-			galaxies, err := util.ListFoldersIn(createIn)
+			galaxies, err := util.ListFoldersIn(folder)
 			if err != nil {
 				return "", err
 			}
@@ -42,40 +51,62 @@ func File() (string, error) {
 			err = survey.AskOne(&survey.Select{
 				Message: fmt.Sprintf(
 					"Which galaxy do you want to create it in from %v?",
-					color.BlueString(createIn+"/"),
+					color.BlueString(folder+"/"),
 				),
 				Options: galaxies,
 			}, &selectedGalaxy)
 			if err != nil {
 				return "", err
 			}
-			createIn = filepath.Join(createIn, selectedGalaxy)
+			folder = filepath.Join(folder, selectedGalaxy)
 		}
-		location, err = createMarkdownFile(createIn)
+
+		location, err = createMarkdownFile(markdownFile{
+			folder: folder,
+		})
 		if err != nil {
 			return "", err
 		}
 	case "nebula":
-		return "", errors.New("Sorry nebulas this is currently no supported")
+		now := time.Now()
+		location, err = createMarkdownFile(
+			markdownFile{
+				folder: filepath.Join("nebulas", fmt.Sprint(now.Year()), now.Month().String()),
+				name:   now.Format(time.Kitchen) + ".md",
+			},
+		)
+		if err != nil {
+			return "", err
+		}
 	}
 
 	return location, nil
 }
 
 // Ask the user what they want to call the file and then create it
-func createMarkdownFile(location string) (string, error) {
-	// Asking for the name of the file
-	var name string
-	err := survey.AskOne(&survey.Input{
-		Message: "Name of the file (without file extension)",
-	}, &name)
-	if err != nil {
-		return "", err
+func createMarkdownFile(data markdownFile) (string, error) {
+	// Asking for the name of the file if needed
+	if data.name == "" {
+		err := survey.AskOne(&survey.Input{
+			Message: "Name of the file (without file extension)",
+		}, &data.name)
+		if err != nil {
+			return "", err
+		}
+		data.name = data.name + ".md"
+	}
+
+	// Creating the folder if it doesn't exist already
+	if _, err := os.Stat(data.folder); os.IsNotExist(err) {
+		err = os.MkdirAll(data.folder, 0777)
+		if err != nil {
+			return "", nil
+		}
 	}
 
 	// Making the actual markdown file
-	location = filepath.Join(location, name+".md")
-	err = os.WriteFile(location, []byte{}, 0655)
+	location := filepath.Join(data.folder, data.name)
+	err := os.WriteFile(location, data.binary, 0655)
 	if err != nil {
 		return "", err
 	}
