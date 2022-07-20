3ec8444de82783ba807d134f832581eb94436790
Matt Gleich <git@mattglei.ch>
2022-07-20 15:51:00 -0400

    feat: remove auto title case
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/cmd/note.rs
+++ src/cmd/note.rs
@@ -1,5 +1,4 @@
 use chrono::Local;
-use inflections::Inflect;
 use printpdf::Mm;
 use rocket::serde::json::Json;
 use rocket::serde::Deserialize;
@@ -21,21 +20,27 @@ pub struct InputData {
 #[post("/note", data = "<inputs>")]
 pub fn route(_token: NeptuneToken, inputs: Json<InputData>) -> Result<String> {
     let now = Local::now();
-    let folder = inputs.folder.to_title_case();
-    let name = inputs.name.to_title_case();
-    let document = Document::new(&name).expect("Failed to create document");
+    let document = Document::new(&inputs.name).expect("Failed to create document");
     write::lines(&document.black_layer, true, 24);
     write::name(&document, Mm(15.0), Mm(800.0));
     write::logo(&document, Mm(480.0), Mm(750.0), 0.50).expect("Failed to write logo to document");
     document
         .black_layer
         .use_text("Note", 50.0, Mm(15.0), Mm(780.0), &document.title_font);
-    document
-        .black_layer
-        .use_text(&folder, 50.0, Mm(15.0), Mm(765.0), &document.title_font);
-    document
-        .black_layer
-        .use_text(&name, 50.0, Mm(15.0), Mm(750.0), &document.title_font);
+    document.black_layer.use_text(
+        &inputs.folder,
+        50.0,
+        Mm(15.0),
+        Mm(765.0),
+        &document.title_font,
+    );
+    document.black_layer.use_text(
+        &inputs.name,
+        50.0,
+        Mm(15.0),
+        Mm(750.0),
+        &document.title_font,
+    );
     document.black_layer.use_text(
         now.format("%D").to_string(),
         50.0,
@@ -53,7 +58,7 @@ pub fn route(_token: NeptuneToken, inputs: Json<InputData>) -> Result<String> {
         25,
     );
     document
-        .upload(format!("/Notes/{}", &folder))
+        .upload(format!("/Notes/{}", &inputs.folder))
         .expect("Failed to upload document");
-    Ok(format!("Created {} in {}", name, folder))
+    Ok(format!("Created {} in {}", inputs.name, inputs.folder))
 }
