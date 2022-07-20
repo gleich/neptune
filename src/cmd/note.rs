e2a34afa356225b8b71656917e1181cd3cfdef26
Matt Gleich <git@mattglei.ch>
2022-07-19 23:41:34 -0400

    feat: get inputs from json body
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/cmd/note.rs
+++ src/cmd/note.rs
@@ -1,5 +1,7 @@
 use chrono::Local;
 use printpdf::Mm;
+use rocket::serde::json::Json;
+use rocket::serde::Deserialize;
 
 use crate::{
     auth::NeptuneToken,
@@ -8,22 +10,37 @@ use crate::{
     write,
 };
 
-#[post("/note?<folder>&<name>")]
-pub fn route(_token: NeptuneToken, folder: String, name: String) -> Result<String> {
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct InputData {
+    pub name: String,
+    pub folder: String,
+}
+
+#[post("/note", data = "<inputs>")]
+pub fn route(_token: NeptuneToken, inputs: Json<InputData>) -> Result<String> {
     let now = Local::now();
-    let document = Document::new(&name).expect("Failed to create document");
+    let document = Document::new(&inputs.name).expect("Failed to create document");
     write::lines(&document.black_layer, true, 24);
     write::name(&document, Mm(15.0), Mm(800.0));
     write::logo(&document, Mm(480.0), Mm(750.0), 0.50).expect("Failed to write logo to document");
     document
         .black_layer
         .use_text("Note", 50.0, Mm(15.0), Mm(780.0), &document.title_font);
-    document
-        .black_layer
-        .use_text(&folder, 50.0, Mm(15.0), Mm(765.0), &document.title_font);
-    document
-        .black_layer
-        .use_text(&name, 50.0, Mm(15.0), Mm(750.0), &document.title_font);
+    document.black_layer.use_text(
+        &inputs.folder,
+        50.0,
+        Mm(15.0),
+        Mm(765.0),
+        &document.title_font,
+    );
+    document.black_layer.use_text(
+        &inputs.name,
+        50.0,
+        Mm(15.0),
+        Mm(750.0),
+        &document.title_font,
+    );
     document.black_layer.use_text(
         now.format("%D").to_string(),
         50.0,
@@ -41,7 +58,7 @@ pub fn route(_token: NeptuneToken, folder: String, name: String) -> Result<Strin
         25,
     );
     document
-        .upload(format!("/Notes/{}", folder))
+        .upload(format!("/Notes/{}", inputs.folder))
         .expect("Failed to upload document");
-    Ok(format!("Created {} in {}", name, folder))
+    Ok(format!("Created {} in {}", inputs.name, inputs.folder))
 }
