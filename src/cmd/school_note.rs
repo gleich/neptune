a35e5ac8ccd02c5dc8931d445554cd1f1f21357b
Matt Gleich <git@mattglei.ch>
2022-10-05 01:31:36 -0400

    feat: have school note cmd use options
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/cmd/school_note.rs
+++ src/cmd/school_note.rs
@@ -2,27 +2,28 @@ use std::path::PathBuf;
 
 use dialoguer::theme::ColorfulTheme;
 use dialoguer::{Input, Select};
-use strum::VariantNames;
-use strum_macros::{EnumVariantNames, FromRepr};
 
 use crate::cli::DIALOGUER_THEME;
 use crate::cmd::note;
+use crate::options::{Class, Options};
 
 pub fn cli_run() {
+	let options = Options::read().unwrap();
 	let theme: &ColorfulTheme = &*DIALOGUER_THEME;
 	let name: String = Input::with_theme(theme)
 		.with_prompt("Name")
 		.interact_text()
 		.expect("Failed to ask user for document name");
-	let class = Class::from_repr(
-		Select::with_theme(theme)
-			.with_prompt("Class")
-			.items(Class::VARIANTS)
-			.default(0)
-			.interact()
-			.expect("Failed to ask user for class"),
-	)
-	.unwrap();
+	let class: &Class = options
+		.classes
+		.get(
+			Select::with_theme(theme)
+				.with_prompt("Class")
+				.items(options.classes.as_slice())
+				.interact()
+				.expect("Failed to ask user for class"),
+		)
+		.unwrap();
 	let folder: String = Input::with_theme(theme)
 		.with_prompt("Folder")
 		.allow_empty(true)
@@ -33,38 +34,11 @@ pub fn cli_run() {
 		&class.to_string(),
 		PathBuf::from("College")
 			.join("Notes")
-			.join(class.short_name())
+			.join(&class.id)
 			.join(folder)
 			.to_str()
 			.unwrap()
 			.to_string(),
 	)
 	.unwrap();
-}
-
-#[derive(EnumVariantNames, FromRepr)]
-pub enum Class {
-	MATH171,
-	MEDG101,
-	PHIL101,
-}
-
-impl ToString for Class {
-	fn to_string(&self) -> String {
-		match self {
-			Self::MATH171 => format!("{}: Calculus A", self.short_name()),
-			Self::MEDG101 => format!("{}: Human Biology 1", self.short_name()),
-			Self::PHIL101 => format!("{}: Intro to Philosophy", self.short_name()),
-		}
-	}
-}
-
-impl Class {
-	pub fn short_name(&self) -> String {
-		match self {
-			Self::MATH171 => String::from("MATH 171"),
-			Self::MEDG101 => String::from("MEDG 110"),
-			Self::PHIL101 => String::from("PHIL 101"),
-		}
-	}
 }
--- src/options.rs
+++ src/options.rs
@@ -39,6 +39,12 @@ impl Options {
 	}
 }
 
+impl Display for Class {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		write!(f, "{}: {}", self.id, self.name)
+	}
+}
+
 impl Display for Book {
 	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
 		write!(f, "{} (by {})", self.name, self.author)
