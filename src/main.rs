b048a2c6a9dfc0e0cc179952a3d69d1f5f638e24
Matt Gleich <git@mattglei.ch>
2022-11-27 19:19:17 -0500

    feat: chdir to neptune directory first
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use cmd::{book_note, school_note};
 
 mod cli;
@@ -6,6 +8,7 @@ mod document;
 mod options;
 
 fn main() {
+	env::set_current_dir("/Users/matt/src/neptune").expect("Failed to change path to repo path");
 	let matches = cli::setup().get_matches();
 	match matches.subcommand() {
 		Some(("school-note", _)) => school_note::cli_run(),
