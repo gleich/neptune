1c3cf6edc073a9a827fcc0ef9658497807ec1290
Matt Gleich <git@mattglei.ch>
2023-11-10 17:55:50 -0500

    feat: remove unused template image func
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/templates/mod.rs
+++ src/templates/mod.rs
@@ -1,17 +1,1 @@
-use std::path::PathBuf;
-
-use anyhow::{Context, Result};
-use genpdf::{elements, Document, Position, Scale};
-
 pub mod note;
-
-pub fn insert_template_image(document: &mut Document, name: String) -> Result<()> {
-	let path = PathBuf::from("assets").join(name);
-	document.push(
-		elements::Image::from_path(&path)
-			.context(format!("Failed to load template image: {}", path.display()))?
-			.with_position(Position::new(0, -12))
-			.with_scale(Scale::new(2.1, 2.1)),
-	);
-	Ok(())
-}
